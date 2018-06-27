13d6461ec247459f97056cb9aa5ae4a1f79aeb7c
naveent221 <31204689+naveent221@users.noreply.github.com>
2018-06-27 10:31:05 +0530

    day-2 updated

---
+++ week-1/Day2/Solution1.java
@@ -0,0 +1,77 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class Solution1 {
+
+    public static int changePossibilities(int amount, int[] denominations) {
+        int[] change = new int[amount+1];
+        Arrays.fill(change, 0);   
+        change[0] = 1;
+        for (int i=0; i<denominations.length; i++)
+            for (int j=denominations[i]; j<=amount; j++)
+                change[j] += change[j-denominations[i]];
+ 
+        return change[amount];
+
+        
+    }
+
+    // tests
+
+    @Test
+    public void sampleInputTest() {
+        final int actual = changePossibilities(4, new int[] {1, 2, 3});
+        final int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneWayToMakeZeroCentsTest() {
+        final int actual = changePossibilities(0, new int[] {1, 2});
+        final int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noWaysIfNoCoinsTest() {
+        final int actual = changePossibilities(1, new int[] {});
+        final int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void bigCoinValueTest() {
+        final int actual = changePossibilities(5, new int[] {25, 50});
+        final int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void bigTargetAmountTest() {
+        final int actual = changePossibilities(50, new int[] {5, 10});
+        final int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void changeForOneDollarTest() {
+        final int actual = changePossibilities(100, new int[] {1, 5, 10, 25, 50});
+        final int expected = 292;
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}

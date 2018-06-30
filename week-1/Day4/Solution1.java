6c2d41c0a27cf3dd03821514a96d3b8454ab40cb
naveent221 <31204689+naveent221@users.noreply.github.com>
2018-06-30 08:11:08 +0530

    day-4 and day-5 updated

---
+++ week-1/Day4/Solution1.java
@@ -0,0 +1,160 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution1 {
+
+    public static class BinaryTreeNode {
+
+        public int value;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+
+        public BinaryTreeNode(int value) {
+            this.value = value;
+        }
+
+        public BinaryTreeNode insertLeft(int leftValue) {
+            this.left = new BinaryTreeNode(leftValue);
+            return this.left;
+        }
+
+        public BinaryTreeNode insertRight(int rightValue) {
+            this.right = new BinaryTreeNode(rightValue);
+            return this.right;
+        }
+    }
+
+    public static int findSecondLargest(BinaryTreeNode root) {
+        // System.out.println(root.value);
+        BinaryTreeNode curr=root;
+        BinaryTreeNode prev=root;
+
+        // find the second 3largest item in the binary search tree
+        
+        if(root==null)
+            throw new IllegalArgumentException("Empty node");
+        
+        if(root.left==null&&root.right==null)
+            throw new IllegalArgumentException("Noleft and right nodes");
+
+        //Go to the largest node
+        while(curr.right != null){
+            prev = curr;
+            curr= curr.right;
+        }
+        //If largest Node has left child, Then largest element of tree with its root as largest.left will be the second largest number.
+        if(curr.left == null)
+            return prev.value;
+        else
+            return findLargest(curr.left);
+        // return 0;
+    }
+    public static int findLargest(BinaryTreeNode root){
+        // No need toi check for null condition as in getSecondLargest method, its already done.
+        BinaryTreeNode curr=root;
+        //To find largest just keep on going to right child till leaf is encountered.
+        while(curr.right != null){
+            curr = curr.right;
+        }
+        return curr.value;
+        
+    }
+
+
+    // tests
+
+    @Test
+    public void findSecondLargestTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        final BinaryTreeNode b = root.insertRight(70);
+        b.insertLeft(60);
+        b.insertRight(80);
+        final int actual = findSecondLargest(root);
+        final int expected = 70;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void largestHasLeftChildTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        root.insertRight(70).insertLeft(60);
+        final int actual = findSecondLargest(root);
+        final int expected = 60;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void largestHasLeftSubtreeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        final BinaryTreeNode b = root.insertRight(70).insertLeft(60);
+        b.insertLeft(55).insertRight(58);
+        b.insertRight(65);
+        final int actual = findSecondLargest(root);
+        final int expected = 65;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void secondLargestIsRootNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        root.insertRight(70);
+        final int actual = findSecondLargest(root);
+        final int expected = 50;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void descendingLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        root.insertLeft(40).insertLeft(30).insertLeft(20);
+        final int actual = findSecondLargest(root);
+        final int expected = 40;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ascendingLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        root.insertRight(60).insertRight(70).insertRight(80);
+        final int actual = findSecondLargest(root);
+        final int expected = 70;
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithTreeThatHasOneNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        findSecondLargest(root);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyTreeTest() {
+        findSecondLargest(null);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
---
+++ week-1/Day5/Solution1.java
@@ -0,0 +1,79 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import java.util.*;
+import static org.junit.Assert.*;
+
+public class Solution1 {
+
+    public static int findRotationPoint(String[] words) {
+
+        // find the rotation point in the array
+       
+        int n=words.length;
+        int low=0,high=n-1,index=0;
+        while(high-low>1)
+        {
+            if(words[low].compareTo(words[high])>0)
+            {
+                int mid=(low+high)/2;
+                if(words[mid].compareTo(words[high])>0)
+                    low=mid;
+                else
+                    high=mid;
+            }
+            else
+                return low;
+            
+            
+        }
+        if(words[low].compareTo(words[high])<=0)
+            index=low;
+        else
+            index=high;
+        return index;
+            
+    }
+
+    // tests
+
+    @Test
+    public void smallArrayTest() {
+        final int actual = findRotationPoint(new String[] {"cape", "cake"});
+        final int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mediumArrayTest() {
+        final int actual = findRotationPoint(new String[] {"grape", "orange", "plum",
+            "radish", "apple"});
+        final int expected = 4;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void largeArrayTest() {
+        final int actual = findRotationPoint(
+            new String[] {"ptolemaic", "retrograde", "supplant", "undulate", "xenoepist",
+            "asymptote", "babka", "banoffee", "engender", "karpatka", "othellolagkage"});
+        final int expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void possiblyMissingEdgeCaseTest() {
+        // are we missing any edge cases?
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}

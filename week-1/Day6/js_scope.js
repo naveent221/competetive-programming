7d71accaa47f1977b31021c23be113b035ad8016
naveent221 <31204689+naveent221@users.noreply.github.com>
2018-07-03 15:10:16 +0530

    day-6 updated

---
+++ week-1/Day6/js_scope.js
@@ -0,0 +1,16 @@
+var text = 'outside';
+function logIt(){
+    console.log(text);
+    var text = 'inside';
+};
+logIt();
+
+
+
+var text = 'outside';
+function logIt(){
+    var text ;
+    console.log(text);
+    text = 'inside';
+};
+logIt();
